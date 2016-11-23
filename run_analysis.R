a65aa4b72b9e5167241384d6820a028ac91a4f5a
Ray Lugo, Jr <rlugojr@gmail.com>
2016-11-23 04:01:07 -0500

    Script modifications
    Subset main data tables to lessen the columns for further processing.
    CBind columns from the data sets
    RBind rows from the two resulting data sets
    Joined the values from the activity table.

--- run_analysis.R
+++ run_analysis.R
@@ -33,35 +33,51 @@ loadfiles <- gsub("UCI HAR Dataset/(test/|train/)?","",datafiles)
 #load each file into its own table
 for (loadfile in loadfiles) {
     if (loadfile == "features.txt") {
-        tblHeader <- read.delim(paste("./data/",loadfile, sep = ""), header = FALSE, sep = " ", stringsAsFactors = FALSE, col.names = c("num","name"))
+        tblHeader <- read.delim(paste0("./data/",loadfile), header = FALSE, sep = " ", stringsAsFactors = FALSE, col.names = c("num","name"))
     } else {
 
         tblname <- tolower(gsub(".txt","",loadfile))
-        assign(tblname, fread(paste("./data/",loadfile, sep = ""), header = FALSE))
+        assign(tblname, fread(paste0("./data/",loadfile), header = FALSE))
     }
 }
 
 rm("url","sourceFile","datafiles","loadfiles","loadfile","tblname")
 
+#get column names from headers table
+allColNames <- tblHeader$name
+
 #add column headers to "x" tables
-colnames(x_test) <- tblHeader$name
-colnames(x_train) <- tblHeader$name
+colnames(x_test) <- allColNames
+colnames(x_train) <- allColNames
 
 #add colmn name to "y" tables
 colnames(y_test) <- "activityID"
 colnames(y_train) <- "activityID"
 
-#add colmn name to "y" tables
-colnames(subject_test) <- "SubjectID"
-colnames(subject_train) <- "SubjectID"
+#add colmn name to "subject" tables
+colnames(subject_test) <- "subjectID"
+colnames(subject_train) <- "subjectID"
 
-testData <- data.table(subject_test, x_test, y_test)
-trainData <- data.table(subject_train, x_train, y_train)
+#add column name to activity_labels
+colnames(activity_labels)<-c("activityID", "activityName")
 
-dtAll <- rbind(testData, trainData)
+#isolate column names to keep.
+keepCols <- x_test[,grep("(mean[^F]|std)",names(x_test))]
+
+#subset tables using column names to keep
+x_test_keep <- select(x_test, keepCols)
+x_train_keep <- select(x_train, keepCols)
+
+testData <- cbind(subject_test, x_test_keep)
+testData <- cbind(testData, y_test)
+trainData <- cbind(subject_train, x_train_keep)
+trainData <- cbind(trainData, y_train)
+
+dtTemp <- rbind(testData, trainData)
+
+dtAll <- inner_join(dtTemp, activity_labels, by = "activityID" )
 
 rm("subject_test","subject_train","tblHeader","testData","trainData","x_test","x_train","y_test","y_train")
-
 
 
 
