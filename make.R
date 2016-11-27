7dd9523be5768ab98c96e7d6a83bf86257bd26ba
Ray Lugo, Jr <rlugojr@gmail.com>
2016-11-26 20:30:24 -0500

    added make.R to automate the tidy data and casebook MD file scripts.

---
+++ make.R
@@ -0,0 +1,13 @@
+
+
+
+source(file = "run_analysis.R", verbose = TRUE, keep.source = TRUE)
+
+source(file = "build_codebook.R", verbose = TRUE, keep.source = TRUE)
+
+
+#library(rmarkdown)
+
+#render("README.Rmd")
+#render("run_analysis.Rmd")
+#render("codebook.Rmd")
